2ebe2ea9b23708e1e65ef4683608f562f142d3fc
smhrd <smhrd@DESKTOP-C58G7I1>
2021-10-21 12:46:39 +0900

    수정

--- src/MusicController.java
+++ src/MusicController.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import ex01.Music;
+import ¿¬½À.MusicVO;
 
 public class MusicController {
 	Scanner sc = new Scanner(System.in);
@@ -10,12 +11,16 @@ public class MusicController {
 	
 	int index = 0;
 	
+	
+	
 	public MusicController() {
-		musiclist.add(new MusicVO(??))
+		musiclist.add(new MusicVO("À¯¸®±¸½½", "¿©ÀÚÄ£±¸", 320));
+		musiclist.add(new MusicVO("¹ð¹ð¹ð", "ºò¹ð", 201));
+		musiclist.add(new MusicVO("ÁÖÀÎ°ø", "¼±¹Ì", 220));
 	}
 	
 	public void List() {
-		for(int i=0 ; array.length<??)
+		for(int i=0 ; musiclist.size(); i++)
 		System.out.println("");
 	}
 	
@@ -28,16 +33,17 @@ public class MusicController {
 		if(index > 0) {
 			index--;
 		}else {
-			index = musiclist.size()-1;
+			index = 0;
 		}
 		return musiclist.get(index);
 		}
 	
 	public MusicVO next() {
+		
 		if(index < musiclist.size()-1) {
 			index++;
 		}else {
-			index = 0;
+			index = musiclist.size();
 		}
 		return musiclist.get(index);
 	}
